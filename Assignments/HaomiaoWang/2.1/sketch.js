f5190491d324122b1fa04c8f9a58e023ed994b58
hmw17 <71084426+hmw17@users.noreply.github.com>
2021-09-30 23:54:45 -0700

    Update sketch.js

--- Assignments/HaomiaoWang/2.1/sketch.js
+++ Assignments/HaomiaoWang/2.1/sketch.js
@@ -8,26 +8,32 @@ let randomIndex;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(200);
+  background('#FFF0F5');
+  textSize(45);
 
-  // cats[0]="Melinda Tang";
-  // cats[1]="Caroline Huang";
-  // cats[2]="Momo Wang";
+  text("click to randomize and see my cats!",500,600);
+
   
   
 }
 
 function draw() {
-  background(20);
-  fill('#E9967A');
-  ellipse(100,100,40);
+  // background(20);
+  // fill('#E9967A');
+  // ellipse(100,100,40);
 }
 
 function mousePressed(){
-  background(random('#00BFFF','#1E90FF'));
-  randomIndex = int(random(cats.length));
+  if (cats[0]){
+    background(random('#00BFFF','#1E90FF'));
+    randomIndex = int(random(cats.length));
   //console.log(cats[randomIndex].name);
-  text(cats[randomIndex].name,60,60);
-  cats.splice(randomIndex,1);
+    text(cats[randomIndex].name,500,600);
+    cats.splice(randomIndex,1);
+  }
+  else{
+    background(random('#00BFFF','#1E90FF'));
+    text("That's all and thank you!",500,600);
+  }
   
 }
