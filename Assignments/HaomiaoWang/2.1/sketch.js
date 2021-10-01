db1ca1d6c9bc4af5da1442bf48947b0f34fd6f7a
hmw17 <71084426+hmw17@users.noreply.github.com>
2021-09-30 23:43:42 -0700

    Update sketch.js

--- Assignments/HaomiaoWang/2.1/sketch.js
+++ Assignments/HaomiaoWang/2.1/sketch.js
@@ -1,9 +1,33 @@
+//I love Cats!
+let cats = [{name:"Melinda", color:"yellow"},
+  {name:"Caroline",color:"blue"},
+  {name:"Momo", color:"pink"}];
+
+let randomIndex;
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(200);
+
+  // cats[0]="Melinda Tang";
+  // cats[1]="Caroline Huang";
+  // cats[2]="Momo Wang";
+  
+  
 }
 
 function draw() {
   background(20);
-  fill(220);
+  fill('#E9967A');
   ellipse(100,100,40);
+}
+
+function mousePressed(){
+  background(random('#00BFFF','#1E90FF'));
+  randomIndex = int(random(cats.length));
+  //console.log(cats[randomIndex].name);
+  text(cats[randomIndex].name,60,60);
+  cats.splice(randomIndex,1);
+  
 }
