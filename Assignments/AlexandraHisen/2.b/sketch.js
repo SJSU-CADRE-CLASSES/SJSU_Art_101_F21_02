73140eeb3ed65912e7895c1f309423fd3770cc81
hisenator <alexandra.hisen@gmail.com>
2021-10-05 23:49:37 -0700

    new things

--- Assignments/AlexandraHisen/2.b/sketch.js
+++ Assignments/AlexandraHisen/2.b/sketch.js
@@ -1,7 +1,18 @@
+let alphabet = ["a","b","c","d","e"]
+
+
 function setup() {
-  createCanvas(400, 400);
+  cnv= createCanvas (400,400);
+  background (200)
+  cnv.mouseClicked(pick);
+  letter = ""
 }
 
 function draw() {
   background(220);
+  text(letter, 50, 50)
+}
+
+function pick() {
+  letter = (alphabet[int(random(alphabet.length))])
 }
