7f30ab5dd053a614ad990a8a902c5ad0b541d71c
CreativeCanoy <80431313+CreativeCanoy@users.noreply.github.com>
2021-10-05 20:21:43 -0700

    Finished 2b.8

--- Assignments/StevenCanoy/2b.6/sketch.js
+++ Assignments/StevenCanoy/2b.6/sketch.js
@@ -16,7 +16,8 @@ let dogs = [{
 }];
 
 let randomIndex;
-let counter = 0;
+let animating = false;
+
 
 function setup() {
   createCanvas(600, 600);
@@ -25,36 +26,39 @@ function setup() {
 
   text("click to randomize", 50, 50); 
 
-  setTimeout(changeBackground, 1000);
+
 }
 
 
 function draw() {
+
+  if(animating == true){
+     ellipse(random(width), random(height), random(50,200));
+  }
   
 }
 
-function changeBackground(){
-  if (counter <= 5){
-    counter++;
-  background(random(255),random(255),random(255));
-  setTimeout(changeBackground, 1000);
-} else {
-
+function randomizer(){
+  animating = false;
+  if (dogs[0]){
+  
+    //random name and splices
+    background(random(200,255));
+    randomIndex = int(random(dogs.length));
+    text(`${dogs[randomIndex].name}'s favorite color is ${dogs[randomIndex].color}`, 50, 50);
+    dogs.splice(randomIndex, 1);
+  } else {
+    background(random(200,255));
+    text("nothing left!", 50, 50);
   }
 }
 
+
 function mousePressed() {
 
-  if (dogs[0]){
-  
-  //random name and splices
-  background(random(200,255));
-  randomIndex = int(random(dogs.length));
-  text(dogs[randomIndex].name, 50, 50);
-  dogs.splice(randomIndex, 1);
-} else {
-  background(random(200,255));
-  text("nothing left!", 50, 50);
-}
+  animating = true;
+  setTimeout(randomizer, 2000);
 
-}
+
+
+ }
