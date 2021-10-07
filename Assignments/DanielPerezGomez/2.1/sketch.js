871a7ee5c1a5ea724193b1213399b3e29be848e9
DanielPG21 <daniel.perezgomez@sjsu.edu>
2021-10-07 14:21:34 -0700

    Update code

--- Assignments/DanielPerezGomez/2.1/sketch.js
+++ Assignments/DanielPerezGomez/2.1/sketch.js
@@ -14,7 +14,7 @@ let music = [{
   artist: "Whitesnake",
   song: "Fool for Your Loving"
 }, {
-artist: "AC/DC",
+  artist: "AC/DC",
   song: "Dirty Deeds Done Dirt Cheap"
 }, {
   artist: "Elvis Presley",
@@ -22,21 +22,66 @@ artist: "AC/DC",
 }];
 
 let randomIndex;
+let animation = false;
+myFontOne = loadFont('assets/Poppins-Bold.ttf');
+
+// let counter = 0;
 
 function setup() {
   createCanvas(600, 600);
   background(255);
+  
+  // setTimeout(changeBackground, 5000)
 }
 
 function draw() {
-
+  fill(random(255), random(255), random(255));
+  if(animation == true) {
+    ellipse(random(width), random(height), random(50, 200));
+  }
 }
 
-function mousePressed() {
+function changeBackground() {
+//   if (counter <= 5){
+//     couner++;
+//   background(random(255), random(255), random(255));
+//   setTimeout(changeBackground, 5000)
+// } else {
+// }
+}
+
+function randomizer(){
+  animation = false;
+  if (music[0]){
   background(random(200, 255));
   randomIndex = int(random(music.length));
   fill('#000000');
+  textFont(myFontOne);
   textSize(40);
-  text(music[randomIndex].artist, 200, 200)
+  textAlign(CENTER);
+  text(`${music[randomIndex].artist}
+  ${music[randomIndex].song}`, width/2, 200)
   music.splice(randomIndex, 1);
+} else {
+  background(random(200, 255));
+  fill('#000000');
+  textFont(myFontOne);
+  textSize(40);
+  textAlign(CENTER);
+  text('Explore New Lands!', width/2, 200)
+}
+}
+
+function mousePressed() {
+  animation = true;
+  setTimeout(randomizer, 2000);
+//   if (music[0]){
+//   background(random(200, 255));
+//   randomIndex = int(random(music.length));
+//   text(music[randomIndex].artist, 200, 200)
+//   music.splice(randomIndex, 1);
+// } else {
+//   background(random(200, 255));
+//   text('Explore New Lands!', 150, 200)
+// }
 }
