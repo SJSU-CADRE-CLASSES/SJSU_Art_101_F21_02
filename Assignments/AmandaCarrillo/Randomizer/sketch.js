1df9fe95a1e0abc889ad6b04ffc3cb8ea939bfd8
amandacarrillo <amanda.carrillo@sjsu.edu>
2021-10-12 15:23:48 -0700

    adding files

--- Assignments/AmandaCarrillo/Randomizer/sketch.js
+++ Assignments/AmandaCarrillo/Randomizer/sketch.js
@@ -1,10 +1,5 @@
-let dogs = ["dillon", "ziggy", "mercedes", "nala", "kloot", "petunia"];
-
 function setup() {
-  createCanvas(600, 600);
-  background(200);
-
-  console.log(dogs[2]);
+  createCanvas(400, 400);
 }
 
 function draw() {
