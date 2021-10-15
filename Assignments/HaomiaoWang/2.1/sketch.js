a399f5952f5aa0f48dd9b3514f952751c8b33d1c
hmw17 <71084426+hmw17@users.noreply.github.com>
2021-10-14 21:23:45 -0700

    2.c10

--- Assignments/HaomiaoWang/2.1/sketch.js
+++ Assignments/HaomiaoWang/2.1/sketch.js
@@ -6,8 +6,9 @@ let cats = [{name:"Melinda", color:"yellow"},
 let randomIndex;
 //let counter = 0;
 let animating = true;
-//let img;
+let imageCounter=0;
 let cats = [];
+let button;
 
 function preload(){
   for (let i=0; i<= 7; i++){
@@ -20,10 +21,17 @@ function preload(){
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background('#FFF0F5');
+  textFont('Courier new');
+  textStyle(BOLD);
+  fill();
   textSize(45);
+  imageMode(CENTER);
+  frameRate(5);
 
   text("click to randomize and see my cats!",500,600);
 
+  button = createButton("click to enjoy cats!")
+  button.mousePressed(buttonPressed);
   //setInterval(changeBackground, 1000);
   //setTimeout(changeBackground, 1000);
   console.log(cats);
@@ -35,7 +43,18 @@ function draw() {
   // ellipse(100,100,40);
 
   if(animating == true){
-    ellipse(random(width), random(high), random(50,100));
+    //ellipse(random(width), random(high), random(50,100));
+    clear();
+
+    image(cats[imageCounter],width/2,height/2);
+    if(imageCounter<cats.length-1){
+      imageCounter++;
+      console.log(imageCounter);
+    }
+    else{
+      imageCounter=0;
+    }
+
   }
 }
 
@@ -52,13 +71,16 @@ function draw() {
 function randomizer(){
   animating = false;
   if (cats[0]){
-    background(random('#00BFFF','#1E90FF'));
+    //background(random('#00BFFF','#1E90FF'));
+    clear();
     randomIndex = int(random(cats.length));
   //console.log(cats[randomIndex].name);
     text(`${cats[randomIndex].name}'s favorite color is
-    ${cats[randomIndex].color}`,500,600);
+    ${cats[randomIndex].color}`,width/2,height-100);
     // text(cats[randomIndex].name + "'s favorite color is",
     // cats[randomIndex].color,500,600);
+    image(random(cats),width/2,height/2);
+
     cats.splice(randomIndex,1);
   }
   else{
@@ -66,8 +88,8 @@ function randomizer(){
     text("That's all and thank you!",500,600);
   }
 }
-
-function mousePressed(){
+//mousePressed
+function buttonPressed(){
   animating = true;
   setTimeout(randomizer,2000);
 
