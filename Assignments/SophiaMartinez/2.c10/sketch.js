67525d17ba3463eb1cd34fe524f7404272765125
Symartinez21 <sophia.martinez@sjsu.edu>
2021-10-19 13:29:32 -0700

    she's rough but she's what i got??

--- Assignments/SophiaMartinez/2.c10/sketch.js
+++ Assignments/SophiaMartinez/2.c10/sketch.js
@@ -46,17 +46,18 @@ function setup() {
 }
 
 function draw() {
-
   if (animating == true) {
+    clear();
     noStroke();
-    image(shops[imageCounter], width/2, height/2);
+
     if (imageCounter < shops.length - 1) {
       imageCounter++;
       console.log(imageCounter);
-    } else {
-      imageCounter = 0;
-
     }
+    else {
+      imageCounter = 0;
+    }
+    image(shops[imageCounter], width/2, height/2);
   }
 }
 
@@ -71,9 +72,9 @@ function draw() {
       text(boba[randomIndex].name + "'s sugar level is " + boba[randomIndex].sugar, 250, 450);
       // image(random(shops), width / 2, height / 2.3);
       boba.splice(randomIndex, 1);
+      shops.splice(randomIndex, 1);
       console.log(boba);
     } else {
-
       background(random(255),random(255),random(255));
       text("No boba for you today >:(", width / 2, height / 2);
     }
