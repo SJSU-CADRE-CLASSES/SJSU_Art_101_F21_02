ac3f5df9233c7ae59d12c0ee7e71b00ffcb951a7
PeerlessPeach <89615322+PeerlessPeach@users.noreply.github.com>
2021-10-20 21:59:15 -0700

    adding button

--- Assignments/JackieNguyen/Randomizer/2b.js
+++ Assignments/JackieNguyen/Randomizer/2b.js
@@ -13,12 +13,15 @@ let activities = [{
   activity: "gardening with my mom"
 }];
 
- let randomIndex;
+let randomIndex;
 let animating = false;
 let activities = [];
-function preload(){
+let imageCounter = 0;
+let button;
 
-  for (let i = 0;  i <= 5; i ++){
+function preload() {
+
+  for (let i = 0; i <= 5; i++) {
     activities[i] = loadImage("assets/activity" + i + ".JPG")
   }
 
@@ -28,38 +31,54 @@ function setup() {
   createCanvas(600, 600);
   background(200);
   textSize(32);
+  textFont('Courier new');
+  textAlign(CENTER);
+  textStyle(BOLD);
+  imageMode(CENTER);
+ framRate(8);
 
   text('click to randomize', 50, 50);
 
+button = createButton("click for a hobby")
+button.mousePressed();
+}
+
+function draw() {
+
+  if (animating == true) {
+    clear();
+    image(activities[imageCounter], width / 2, height / 2);
+
+    if (imageCounter < activities.length - 1) {
+      imageCounter++;
+      console.log(imageCounter);
+    } else {
+      imageCounter = 0;
+    }
+  }
+}
+
+function randomizer() {
+  animating = false;
+
+  if (activities[0]) {
+    //this displays a random activity and splices it out of the array
+    //background(random(200, 255));
+    clear();
+    randomIndex = int(random(activities.length));
+    text(activities[randomIndex].activity + " is fun", width/2, height - 50);
+    image(random(activities), width / 2, height / 2);
+    activities.splice(randomIndex, 1);
+  } else {
+    background(random(200, 255));
+    text("nothing left!", 50, 50);
+  }
 
 }
 
-  function draw() {
+function buttonPressed() {
+  animating = true;
+  setTimeout(randomizer, 2000);
 
-    if(animating == true){
-  ellipse(random(width), random(height), random(50, 200));
-
- }
-}
-
- function randomizer(){
-   animating = false;
-
- if (activities[0]){
-   //this displays a random activity and splices it out of the array
-  background(random(200, 255));
- randomIndex = int(random(activities.length));
-  text(activities[randomIndex].activity + " is fun", 50, 50);
-  activities.splice(randomIndex, 1);
- } else {
-   background(random(200, 255));
-   text("nothing left!", 50, 50);
-}
 
 }
- function mousePressed() {
- animating = true;
- setTimeout(randomizer, 2000);
-
-
- }
