c15e240b986c512536208ad1bae3e5a0baafc055
uuoaoue <uyen.p.huynh@sjsu.edu>
2021-10-28 15:47:17 -0700

    create 3.0

---
+++ Assignments/UyenHuynh/3.0/sketch.js
@@ -0,0 +1,13 @@
+// my stuffs
+let stuffs = ["ipad","phone","lipstick","notebook"];
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(240);
+  fill(255, 204, 0);
+  noStroke();
+  circle(100, 100, 100);
+}
