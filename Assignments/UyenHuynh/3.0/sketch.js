baa0f4bf705eec6d5aa759cc2df8125a9cb52455
uuoaoue <uyen.p.huynh@sjsu.edu>
2021-11-02 15:54:05 -0700

    update 3.0

--- Assignments/UyenHuynh/3.0/sketch.js
+++ Assignments/UyenHuynh/3.0/sketch.js
@@ -1,17 +1,36 @@
+let array=[];
+
 function setup(){
-  createCanvas(600,600);
+  let c = createCanvas(windowWidth, windowHeight);
   background(200);
   strokeWeight(5);
-  line(width/2, 0, width/2,height);
-  fill(0);
-  ellipse(width/2, height/2,5,5)
+  noFill();
+
 
 }
 function draw(){
   if (mouseIsPressed){
-    line(width-mouseX, height-mouseY, width-pmouseX,height-pmouseY);
-      line(mouseX, mouseY, pmouseX,pmouseY);
+    // line(width-mouseX, height-mouseY, width-pmouseX,height-pmouseY);
+      // line(mouseX, mouseY, pmouseX,pmouseY);
+      background(0);
 
+      array.push([mouseX, mouseY]);
   }
 
+}
+function keyTyped(){
+  if (key === 's'){
+    saveCanvas('fileName','png');
+  }else if (key ==='d'){
+    background(255);
+    beginShape();
+    for (let i=0; i< array.length; i++){
+      // line(array[i][0],array[i][1],array[i+1][0],array[i+1][1] )
+      curveVertex(array[i][0], array[i][1])
+    } 
+  endShape();
+  }else if ( key === 'c'){
+    background(200);
+  }
+  return false;
 }
