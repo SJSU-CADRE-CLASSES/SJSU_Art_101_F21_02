08a2ed8eb337d5b832d570960937b44c7af0438b
hmw17 <71084426+hmw17@users.noreply.github.com>
2021-11-02 23:59:22 -0700

    1

---
+++ Assignments/HaomiaoWang/3.6/sketch.js
@@ -0,0 +1,32 @@
+let array = [];
+function setup (){
+  createCanvas(600, 600);
+  background(220);
+  strokeWeight(3);
+noFill();
+  }
+
+function draw() {
+
+if (mouseIsPressed){
+  //line(mouseX, mouseY, pmouseX, pmouseY);
+  background(0);
+  array.push([mouseX, mouseY]);
+  }
+
+}
+
+function keyTyped(){
+
+  if (key === 's'){
+    //save image
+    saveCanvas('fileName', 'png');
+  } else if (key === 'd'){
+  //display image
+background(255);
+  for(let i = 0; i < array.length - 1; i++){
+    line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+  }
+}
+  return false;
+}
