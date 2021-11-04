2bceb5bf0480b6a6c68e59f896276ff21783a0ec
uuoaoue <uyen.p.huynh@sjsu.edu>
2021-11-04 15:35:08 -0700

    create 3.14

---
+++ Assignments/UyenHuynh/3.10/sketch.js
@@ -0,0 +1,43 @@
+let array=[];
+let noiseOffset = 0.0;
+let strokeWidth = 5;
+
+let backgroundColor =200;
+
+function setup(){
+ createCanvas(windowWidth, windowHeight);
+  background(backgroundColor);
+  noFill();
+
+
+}
+function draw(){
+  background(200,5);
+  noiseOffset = noiseOffset + 0.01;
+  strokeWeight(strokeWidth);
+  strokeWidth = noise(noiseOffset) * 30;
+  // if (mouseIsPressed){
+     
+      // background(backgroundColor);
+      // backgroundColor -=2;
+      line(mouseX, mouseY, pmouseX,pmouseY);
+      array.push([mouseX, mouseY]);
+  }
+
+// }
+function keyTyped(){
+  if (key === 's'){
+    saveCanvas('fileName','png');
+  }else if (key ==='d'){
+     beginShape();
+    for (let i=0; i< array.length; i++){
+      // line(array[i][0],array[i][1],array[i+1][0],array[i+1][1] )
+      curveVertex(array[i][0], array[i][1])
+    } 
+  endShape();
+  }else if ( key === 'c'){
+   clear();
+
+  }
+  return false;
+}
