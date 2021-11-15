c7036f6149e07ea63d1f8032f783d2f9441e40d1
kennyn408 <47119438+kennyn408@users.noreply.github.com>
2021-11-15 12:32:10 -0800

    Update sketch.js

--- Assignments/KennyNguyen/Drawing Machine/sketch.js
+++ Assignments/KennyNguyen/Drawing Machine/sketch.js
@@ -10,7 +10,7 @@ function setup() {
 function draw() {
 
   if (mouseIsPressed){
-    // line(mouseX, mouseY, pmouseX, pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
     array.push([mouseX, mouseY]);
   }
 
@@ -21,15 +21,7 @@ function keyTyped(){
   if (key === 's'){
     // save this image
     saveCanvas('fileName', 'png');
-  } else if (key === 'a'){
-    // display image
-
-beginShape();
-    for(let i = 0; i < array.length; i++){
-      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-      curvedVertex(array[i][0], array[i][1])
-    }
-    endShape();
+  }
 
   }
   return false;
