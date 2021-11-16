d519ba4cc3c49543c51f771a9bf17fbfbf404e03
JarrylAncheta <55153273+JarrylAncheta@users.noreply.github.com>
2021-11-16 02:19:25 -0800

    3.14 update

--- Assignments/JarrylAncheta/3.14/sketch.js
+++ Assignments/JarrylAncheta/3.14/sketch.js
@@ -1,76 +1,57 @@
+let array = [];
+let strokeWidth = 10
+let noiseOffset = 0.0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(120, 190, 255);
+
+  drawGrid();
+  noFill();
+
 }
 
 function draw() {
-  strokeWeight(11);
-  push();
-  translate(width/5.2,height/2.8);
-  scale(2.2,1.1);
-  line(10, 10, 390, 10);
-  line(50, 50, 350, 50);
-  line(50, 250, 50, 50);
-  line(50, 300, 50, 350);
-  line(10, 390, 50, 390);
-  line(10, 10, 10, 390);
-  line(390, 10, 390, 390);
-  line(10, 390, 300, 390);
-  line(390, 390, 350, 390);
-  line(350, 50, 350, 90);
-  line(350, 150, 350, 350);
-  line(50, 350, 350, 350);
 
-  line(90, 90, 90, 310);
-  line(150, 90, 90, 90);
-  line(190, 90, 310, 90);
-  line(90, 310, 220, 310);
-  line(260, 310, 310, 310);
-  line(310, 90, 310, 310);
+  background(330, 249, 220, 1);
+  strokeWeight(strokeWidth)
 
-  line(130, 130, 130, 200);
-  line(130, 250, 130, 270);
-  line(270, 130, 130, 130);
-  line(130, 270, 270, 270);
-  line(270, 130, 270, 270);
+  noiseOffset += 0.05
+  strokeWidth = noise(noiseOffset) * 10;
 
-  line(170, 170, 170, 230);
-  line(230, 170, 170, 170);
-  line(170, 230, 230, 230);
-  line(230, 170, 230, 230);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  //  array.push([mouseX, mouseY]);
 
-  line(350, 300, 390, 300);
-  line(310, 250, 350, 250);
-  line(90, 250, 50, 250);
-  line(240, 90, 240, 130);
-  line(170, 200, 130, 200);
-  pop();
+}
 
-  background(300,100, 930, 1)
 
-  if (mouseIsPressed){
-    stroke(map(mouseX, 200, 900, 2000, 1000, true))
-    line(mouseX, mouseY, pmouseX, pmouseY);
+function keyTyped() {
+
+  if (key === 's') {
+    saveCanvas('fileName', 'jpg')
+  } else if (key === 'c') {
+    clear();
+  }
+  else if (key === 'd'){
+    display();
+  }
+  return false;
+}
+
+function drawGrid() {
+  numCells = 35;
+  fillColor = 0;
+
+  for (let i = 0; i <= width; i += width / numCells) {
+    for (let j = 0; j <= height; j += height / numCells) {
+if (fillColor === 255){
+  fillColor = 255;
+}else{
+  fillColor = 255;
+}
+    fill(fillColor)
+;      rect(i, j, width / numCells, height / numCells)
 
     }
   }
-
-
-  function keyTyped(){
-
-    if (key === 's'){
-      //save image
-      saveCanvas(`fileName`, `png`);
-    //} else if (key === `d`) {
-      //display image
-    } else if (key ===`c`){
-      //clear image
-      clear();
-
-
-      for(let i = 0; i < array.length - 1; i++){
-    }
-    }
-
-    return false;
-
-  }
+}
