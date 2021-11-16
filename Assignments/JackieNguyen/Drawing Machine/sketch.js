1ca4c06009e52fd071bbffb64a3293c9b9c90c45
PeerlessPeach <89615322+PeerlessPeach@users.noreply.github.com>
2021-11-15 21:28:57 -0800

    adding functions

--- Assignments/JackieNguyen/Drawing Machine/sketch.js
+++ Assignments/JackieNguyen/Drawing Machine/sketch.js
@@ -1,3 +1,5 @@
+let array = [];
+
 function setup() {
   createCanvas(600, 600);
   background(220, 50, 133);
@@ -8,6 +10,22 @@ function setup() {
 function draw() {
 
 if (mouseIsPressed){
-  line(mouseX, mouseY, pmouseX, pmouseY);
+line(mouseX, mouseY, pmouseX, pmouseY);
+array.push([mouseX,  mouseY]);
 }
+
 }
+
+ function keyTyped(){
+
+
+   if (key ==='s'){
+// save this image
+saveCanvas('fileName', 'png');
+} else if (key === 'd'){
+  //display image
+  console.log(array);
+}
+
+   return false;
+ }
