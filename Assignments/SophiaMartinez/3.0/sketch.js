cad84f71158a8e6f824304911b6e0a8def4b1a49
Symartinez21 <sophia.martinez@sjsu.edu>
2021-11-16 13:57:41 -0800

    renamed folder

--- Assignments/SophiaMartinez/3.0/sketch.js
+++ Assignments/SophiaMartinez/3.0/sketch.js
@@ -1,31 +1,30 @@
-let array = [];
-function setup (){
+
+function setup () {
   createCanvas(600, 600);
   background(230);
-  strokeWeight(3);
-noFill();
+  strokeWeight(4);
+// noFill();
   }
 
 function draw() {
 
-if (mouseIsPressed){
-  array.push([mouseX, mouseY]);
-  }
+  // if (mouseIsPressed){
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+background(199, 168, 107);
+line(mouseX, mouseY, pmouseX, pmouseY);
+
+
 
 }
 
-function keyTyped(){
+function keyTyped() {
 
   if (key === 's'){
     //save image
     saveCanvas('fileName', 'png');
-beginShape();
-for (let i = 0; o < array.length; i++) {
-    curveVertex(array[i][0], array[i][1]);
-endShape();
+  } else if (key === 'c') {
+    //clear the image
+    clear();
   }
-background: rgb(199, 168, 107);
-
-}
   return false;
 }
