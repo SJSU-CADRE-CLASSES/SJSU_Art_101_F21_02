750ac9150ea1511d77bcf0edf8478fc99b09ac4c
amandacarrillo <amanda.carrillo@sjsu.edu>
2021-11-15 22:36:16 -0800

    Editing Code

--- Assignments/AmandaCarrillo/3.0/sketch.js
+++ Assignments/AmandaCarrillo/3.0/sketch.js
@@ -1,7 +1,27 @@
+let array = [];
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(255, 204, 0);
+  strokeWeight(7);
+
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed){
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, mouseY]);
+  }
+}
+
+function keyTyped() {
+
+  if (key === 's') {
+    // save this image
+    saveCanvas('Kaleidoscope', 'png');
+  } else if (key === 'd') {
+    //display image
+    console.log(array);
+  }
+  return false;
 }
