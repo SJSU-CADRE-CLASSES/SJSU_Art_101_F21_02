c4e43164681687730104147537207b885e3ab79d
CreativeCanoy <80431313+CreativeCanoy@users.noreply.github.com>
2021-11-16 19:19:45 -0800

    finished drawMachine

--- Assignments/StevenCanoy/drawmachine/sketch.js
+++ Assignments/StevenCanoy/drawmachine/sketch.js
@@ -1,36 +1,44 @@
 let noiseOffset = 0.0;
 let strokeWidth = 5;
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(220, 50, 133);
+  background(220);
   
   
 }
 
 function draw() {
 
-      //Background
-      background(220, 50, 133, 5);
 
       //Stroke Weight
       strokeWeight(strokeWidth);
 
       //Noise Offset
-      noiseOffset += 0.09;
+     noiseOffset += 0.5;
 
       //Stroke Weight Changing
-      strokeWidth = noise(noiseOffset) * 100;
+      strokeWidth = noise(noiseOffset) / 5;
  
 
     //Below changes to color of stroke
-    stroke(map(mouseX, 0, 600, 0, 255, true));
+    stroke(map(mouseX, 0, 300, 0, 100, true));
 
     //Below draws in opposite direction
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
 
-    //Below draws a line
-        line(mouseX, mouseY, pmouseX, pmouseY);
+    
+
+    //Below draws an ellipse
+        push();
+        strokeWeight(2.5);
+        fill(mouseX, mouseY, 150, 275);
+        ellipse(mouseX, mouseY, 150, 170);
+        ellipse(width - mouseX, height - mouseY, 100, 100);
+        
+        //triangle(mouseX, mouseY, width - mouseX, height - mouseY, windowWidth / 2 , windowHeight / 2);
+        pop();
       
    }
 
