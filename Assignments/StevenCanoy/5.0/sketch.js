e4dd3bc14276fff6106e12af93bf4313d7e6b4af
CreativeCanoy <80431313+CreativeCanoy@users.noreply.github.com>
2021-11-23 14:43:54 -0800

    Finished 5.0 and 5.1 and added 5.2

--- Assignments/StevenCanoy/5.0/sketch.js
+++ Assignments/StevenCanoy/5.0/sketch.js
@@ -1,79 +1,9 @@
-let noiseOffset = 0.0;
-let strokeWidth = 5;
+'use strict'
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  //background(220, 50, 133);
-  drawGrid();
-  strokeWeight(5);
-  noFill();
-  
-  
-}
-
-function draw() {
-
-      //Background
-      background(220, 50, 133, 5);
-
-      if (mouseIsPressed) {
-
-      //Stroke Weight
-      strokeWeight(strokeWidth);
-
-      //Noise Offset
-      noiseOffset += 0.09;
-
-      //Stroke Weight Changing
-      strokeWidth = noise(noiseOffset) * 100;
- 
-
-    //Below changes to color of stroke
-    stroke(map(mouseX, 0, 600, 0, 255, true));
-
-    //Below draws in opposite direction
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-
-    //Below draws a line
-        line(mouseX, mouseY, pmouseX, pmouseY);
-
-      }
-      
-   }
-   
-
-
-
-function keyTyped(){
-
-  //Save Image of Canvas
-  if (key ==='s'){
-    saveCanvas('drawmachineimage', 'png');
-  } else if (key === 'c'){
-
-  //Clear Image
-    clear();
-   
-   
+  function setup(){
+    createCanvas(600, 600);
   }
 
-
-return false;
-
-}
-
-function mousePressed() {
-  array = [];
-  backgroundColor = 255;
-}
-
-function drawGrid(){
-  numCells = 20;
-
-  for (let i = 0; i <= width; i += width / numCells){
-    for (let j = 0; j <= height; j += height / numCells){
-    rect(i, j, width / numCells, height / numCells);
-    }
-
+  function draw(){
+    background(100);
   }
-}
