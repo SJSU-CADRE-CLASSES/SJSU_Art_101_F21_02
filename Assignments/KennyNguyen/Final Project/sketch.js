d23bc13cf433155677d494cd7ed25247f12fa8ac
kennyn408 <47119438+kennyn408@users.noreply.github.com>
2021-11-24 13:02:52 -0800

    Update sketch.js

--- Assignments/KennyNguyen/Final Project/sketch.js
+++ Assignments/KennyNguyen/Final Project/sketch.js
@@ -1,17 +1,22 @@
 'use strict';
 
 let state = 'title';
+let cnv;
 
 function setup() {
-  createCanvas(600, 600);
+  cnv = createCanvas(600, 600);
 }
 
 function draw() {
 
   if (state === 'title') {
     title();
+    cnv.mouseClicked(titleMouseClicked);
   } else if (state === 'level 1'){
     level1();
+    cnv.mouseClicked(function(){
+      console.log('canvas is clicked on level 1');
+    });
   }
 }
 
@@ -20,6 +25,14 @@ function title(){
   textSize(80);
   stroke(255);
   text('MY GAME', 100, 100);
+
+  textSize(30);
+  text('click anywhere to start', 100, 300);
+}
+
+function titleMouseClicked(){
+    console.log('canvas is clicked on title page');
+    state = 'level 1'
 }
 
 function level1(){
