fa7a5c8d594a0471039bb233833ae8d10bc5843c
kennyn408 <47119438+kennyn408@users.noreply.github.com>
2021-11-25 11:53:36 -0800

    Update sketch.js

--- Assignments/KennyNguyen/Final Project/sketch.js
+++ Assignments/KennyNguyen/Final Project/sketch.js
@@ -3,6 +3,7 @@
 
 let state = 'title';
 let cnv;
+let points = 0;
 
 function setup() {
   cnv = createCanvas(600, 600);
@@ -19,6 +20,10 @@ function draw() {
     case 'level 1':
       level1();
       cnv.mouseClicked(level1MouseClicked);
+      break;
+    case 'You win!':
+      youWin();
+      cnv.mouseClicked(youWinMouseClicked);
       break;
     default:
       break;
@@ -43,8 +48,29 @@ function titleMouseClicked(){
 
 function level1(){
   background(50, 150, 200);
+  text('click for points', 0, height - 30);
 }
 
 function level1MouseClicked(){
-  console.log('canvas is clicked on level 1');
+  points++;
+  console.log('points = ' + points);
+
+  if (points >= 10){
+    state = 'You win!';
+  }
+}
+
+function youWin(){
+  background(255, 50, 80);
+  textSize(80);
+  stroke(255);
+  text('You win!', 100, 100);
+
+  textSize(30);
+  text('click anywhere to restart', 100, 300);
+}
+
+function youWinMouseClicked(){
+  state = 'level 1';
+  points = 0;
 }
