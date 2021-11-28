2cc056d33cff14c30e5d0ec938b8f1ac484f1dce
uuoaoue <uyen.p.huynh@sjsu.edu>
2021-11-28 14:09:40 -0800

    update

--- Assignments/UyenHuynh/4.0/sketch.js
+++ Assignments/UyenHuynh/4.0/sketch.js
@@ -11,7 +11,7 @@ let coin;
 
 function setup() {
   cnv = createCanvas(w, h);
-  textFont('Courier'); 
+  textFont('Courier');
   player = new Player();
   coin = new Coin();
 
@@ -27,7 +27,7 @@ function draw() {
       level1();
       cnv.mouseClicked(level1MouseClicked);
       break;
-      case 'you win':
+    case 'you win':
       youWin();
       cnv.mouseClicked(youWinMouseClicked);
       break;
@@ -35,15 +35,26 @@ function draw() {
       break;
   }
 }
-
-
+function keyPressed() {
+  if (keyCode == LEFT_ARROW) {
+    player.direction = 'left'
+  } else if (keyCode == RIGHT_ARROW) {
+    player.direction = 'right'
+  } else if (keyCode == UP_ARROW) {
+    player.direction = 'up'
+  } else if (keyCode == DOWN_ARROW) {
+    player.direction = 'down'
+  }else if (keyCode == ' '){
+    player.direction = 'still'
+  }
+}
 function title() {
   background(100);
   textSize(80);
   textAlign(CENTER);
-  text("MY GAME", w/2, h/5);
+  text("MY GAME", w / 2, h / 5);
   textSize(30);
-  text("click anywhere to start", w/2, h/2);
+  text("click anywhere to start", w / 2, h / 2);
 
 }
 
@@ -55,33 +66,34 @@ function titleMouseClicked() {
 
 function level1() {
   background(50, 100, 200);
-  text('click for points',  w/2, h/2);
+  text('click for points', w / 2, h / 2);
   player.display();
+  player.move();
   coin.display();
   coin.move();
 
 }
 
 function level1MouseClicked() {
-  points ++;
-  text('Points = '+ points);
-  console.log("points ="+ points);
-if (points >= 10){
-  state = 'you win'
-}
+  points++;
+  text('Points = ' + points);
+  console.log("points =" + points);
+  if (points >= 10) {
+    state = 'you win'
+  }
 }
 
-function youWin(){
-  background(255,90,100);
+function youWin() {
+  background(255, 90, 100);
   textSize(80);
-  text('You Win',  w/2, h/5);
+  text('You Win', w / 2, h / 5);
   textSize(30);
-  text("click anywhere to restart", w/2, h/2);
+  text("click anywhere to restart", w / 2, h / 2);
 
 
 }
 
-function youWinMouseClicked(){
-state ='level 1';
-points = 0;
+function youWinMouseClicked() {
+  state = 'level 1';
+  points = 0;
 }
