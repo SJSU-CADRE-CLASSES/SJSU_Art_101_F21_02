907d413c403c10e097a851daf25242af1b6036e5
CreativeCanoy <80431313+CreativeCanoy@users.noreply.github.com>
2021-11-30 16:52:49 -0800

    testing poseNet

--- Assignments/StevenCanoy/poseNet/sketch.js
+++ Assignments/StevenCanoy/poseNet/sketch.js
@@ -1,18 +1,34 @@
 let video;
 let poseNet;
+let pose;
+
 
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.hide();
   poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+
+function gotPoses(poses) {
+  console.log(poses);
+  if (poses.length > 0) {
+    pose = pose[0].pose;
+  }
 }
 
 
 function modelLoaded(){
-  console.log('poseNet ready')
+  console.log('poseNet ready');
 }
 
 function draw() {
   image(video, 0, 0);
+
+  if (pose) {
+    fill(255,0,0);
+    ellipse(pose.nose.x, pose.nose.y, 64);
+  }
 }
