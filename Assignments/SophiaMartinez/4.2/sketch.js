7b03a8f9122cba316e6d5706eecaf0ebb57a7532
Symartinez21 <sophia.martinez@sjsu.edu>
2021-12-09 10:17:26 +0100

    changes

--- Assignments/SophiaMartinez/4.2/sketch.js
+++ Assignments/SophiaMartinez/4.2/sketch.js
@@ -1,78 +1,28 @@
-'use strict';
-
-let state = 'title';
-let cnv;
-let points = 0;
-let w = 600;
-let h = 600;
+let video;
+let poseNet;
 
 function setup() {
-  cnv = createCanvas(w, h);
-  textFont('monospace');
-
-}
-
-function draw() {
-
-  switch (state) {
-    case 'title':
-      title();
-      cnv.mouseClicked(titleMouseClicked);
-      break;
-    case 'level 1':
-        level1();
-        cnv.mouseClicked(level1MouseClicked);
-        break;
-      case 'you win':
-      youWin();
-      cnv.mouseClicked(youWinMouseClicked);
-      break;
-      default:
-        break;
-  }
-
-}
-
-function title() {
-  background(100);
-  textSize(40);
-  fill(255);
-  text('FOOLS QUEST', w/2, h/5);
-  textAlign(CENTER);
-  textSize(30);
-  text('click anywhere to start', w/2, h/2);
+  createCanvas(600, 600);
+  video = createCapture(VIDEO);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('post', gotPoses);
 }
 
 
-function titleMouseClicked() {
-  console.log('canvas is clicked on title page');
-  state = 'level 1'
+function gotPoses(poses){
+  console.log(poses);
+
+function modelLoaded() {
+  console.log('poseNet ready');
 }
 
-function level1() {
-  background(50, 150, 200);
-  text('click 4 points', w/2, h - 30);
+
+function draw(){
+  image(video, 0, 0);
+
+if(pose){
+  fill(255 0, 0);
+  ellipse(pose.nose.x, pose.nose.y, 64);
 }
-
-function level1MouseClicked() {
-  points++;
-  console.log('points = '+ points);
-
-  if (points >= 10){
-    state = 'you win';
-  }
-}
-
-function youWin(){
-  background(255, 50, 80);
-  textSize(40);
-  stroke(255);
-  text('YOU WIN', 100, 100);
-  textSize(30);
-  text('click anywhere to restart', w/2, h* 3/4);
-}
-
-function youWinMouseClicked(){
-  state = 'level 1';
-  points = 0;
 }
