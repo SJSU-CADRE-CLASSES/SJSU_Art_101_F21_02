efec2c1c82bec0a94c1962906696301b192fe360
hisenator <alexandra.hisen@gmail.com>
2021-12-09 00:47:09 -0800

    not the beeeeeeees

--- Assignments/AlexandraHisen/4.0/enemies.js
+++ Assignments/AlexandraHisen/4.0/enemies.js
@@ -1,15 +1,16 @@
 
-class Coin {
+class Enemy {
   constructor (){
     this.r = 50
     this.x = random (w);
     this.y = 0 - this.r;
-    
+    this.speed = 7
+
 
   }
 
   display(){
-    image(coinsImg, this.x, this.y, this.r, this.r)
+    image(enemiesImg, this.x, this.y, this.r, this.r)
   }
   move(){
     this.y++;
