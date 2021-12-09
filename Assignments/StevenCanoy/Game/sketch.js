2ab3308472a285f9f7b6236d4fd4803a00cd6405
CreativeCanoy <80431313+CreativeCanoy@users.noreply.github.com>
2021-12-09 09:01:36 -0800

    Added shoofly game

--- Assignments/StevenCanoy/Game/sketch.js
+++ Assignments/StevenCanoy/Game/sketch.js
@@ -1,70 +1,160 @@
-let noiseOffset = 0.0;
-let strokeWidth = 5;
+var img;
+var img2;
+var img3;
+var ballx = 300;
+var bally = 300;
+var ballSize = 40;
+var score =0;
+var gameState = "L1";
+var choice = '1';
 
+
+function preload() {
+// preload() runs once, it may make you wait
+//  img = loadImage('cat.jpg');  // cat.jpg needs to be next to this .js file
+// you can link to an image on your github account
+  img = loadImage('https://creativecanoy.github.io/images/fly2.gif');
+ img2 = loadImage('https://creativecanoy.github.io/images/fly.gif');
+ img3 = loadImage('https://creativecanoy.github.io/images/fly3.gif');
+}
 
 function setup() {
   createCanvas(600, 600);
-  background(22);
+  textAlign(CENTER);
+  textSize(20);
   
-  
-}
+
+} // end setup   ================================================================================================
+
 
 function draw() {
-
-  if (mouseIsPressed) {
-
-      //Stroke Weight
-      strokeWeight(strokeWidth);
-
-      //Noise Offset
-     noiseOffset += 0.5;
-
-      //Stroke Weight Changing
-      strokeWidth = noise(noiseOffset) / 5;
- 
-
-    //Below changes to color of stroke
-    stroke(map(mouseX, 0, 300, 0, 100, true));
-
-    //Below draws in opposite direction
-    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-
-    
-
-    //Below draws an ellipse
-        push();
-        strokeWeight(2.5);
-        //noStroke();
-        fill(mouseX, mouseY, random(255), 275);
-        ellipse(mouseX, mouseY, 150, 170);
-        ellipse(width - mouseX, height - mouseY, 100, 100);
-        pop();
-
+ cursor(HAND);
+   background('blue');
+  if (gameState == "L1"){
+  levelOne();
   }
-      
-   }
+  if (gameState =="L2"){
+    levelTwo();
+  }
+    if (gameState =="L3"){
+    levelThree();
+  }
+   if (gameState =="L4"){
+    levelFour();
+  }
+   if (gameState =="L5"){
+    levelFive();
+  }
+  if (gameState =="L6"){
+    levelSix();
+  }
+text(("score: " + score), width/2, 40);
+} // end draw  =======================================================================================================
+
+function levelOne(){
+  
+  text("Level 1", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  
+  if(distToBall <ballSize/2) {
+   ballx=random(width);
+   bally=random(height);
+    score = score +1;
+  }
+  if (score>=5){
+  gameState ="L2";
+  }
+//  line(ballx, bally, mouseX, mouseY);
+   image(img3, ballx, bally, 50, 50);
+//  ellipse(ballx, bally, ballSize, ballSize);
+  
+} //end level one  ================================================================================================
 
 
+function levelTwo(){
+   background('green');
+  text("Level 2", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  
+  if(distToBall <ballSize/2) {
+   ballx=random(width);
+   bally=random(height);
+    score = score +1;
+  }
+  if (score>=10){
+    gameState ="L3";
+  }
+//  line(ballx, bally, mouseX, mouseY);
+  image(img, ballx, bally, 50, 50);
+//  ellipse(ballx, bally, ballSize, ballSize);
+  
+} //end level two  ================================================================================================
 
-function keyTyped(){
 
-  //Save Image of Canvas
-  if (key ==='s'){
-    saveCanvas('drawmachineimage', 'png');
-  } else if (key === 'c'){
-
-  //Clear Image
-    clear();
-   
-   
+function levelThree(){
+   background('orange');
+  text("Level 3", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  
+  if(distToBall <ballSize/2) {
+   ballx=random(width);
+   bally=random(height);
+    score = score +1;
+//    ballSize= ballSize -1;
+  }
+  if (score>=15){
+    gameState ="L4";
   }
 
+//   ellipse(ballx, bally, ballSize, ballSize);
+ image(img2, ballx, bally, 50, 50);
+  
+} //end level three   ================================================================================================
 
-return false;
 
-}
+function levelFour(){
+   background('purple');
+  text("Level 4", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  
+  if(distToBall <ballSize/2) {
+   ballx=random(width);
+   bally=random(height);
+    score = score +1;
+ //   ballSize= ballSize -1;
+  }
+  if (score>=20){
+    gameState ="L5";
+  }
 
-function mousePressed() {
-  array = [];
-  backgroundColor = 255;
-}
+//   ellipse(ballx, bally, ballSize, ballSize);
+  image(img, ballx, bally, 50, 50);
+  
+} //end level four   ================================================================================================
+
+function levelFive(){
+   background(random(255));
+  text("Level 5", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  
+  if(distToBall <ballSize/2) {
+   ballx=random(width);
+   bally=random(height);
+    score = score +1;
+    ballSize= ballSize -1;
+  }
+  if (score>=25){
+    gameState ="L6";
+  }
+
+//   ellipse(ballx, bally, ballSize, ballSize);
+  image(img, ballx, bally, 50, 50);
+  
+} //end level five   ================================================================================================
+
+
+function levelSix(){
+  
+  text("Winner!", width/2, height-20);
+  
+} //end GAME  ================================================================================================
