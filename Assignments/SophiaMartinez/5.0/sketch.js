98d8ca73460b51da31de0541633726c2dd8a3455
Symartinez21 <sophia.martinez@sjsu.edu>
2021-12-09 15:50:32 +0100

    5.0

---
+++ Assignments/SophiaMartinez/5.0/sketch.js
@@ -0,0 +1,20 @@
+var api = "https://api.giphy.com/v1/gifs/search?";
+var apiKey = "xxe80RCCzSM7bZuMJP6hTGDIs78CjJFc";
+var query = "&q=airport";
+
+function setup(){
+  noCanvas();
+  var url = api + apiKey + query;
+  loadJSON(url, gotData);
+}
+
+function gotData(giphy){
+  for (var i = 0; i < giphy.data.length; i++){
+  createImg(data.data[0].images.original.url);
+}
+}
+
+function draw(){
+
+
+}
